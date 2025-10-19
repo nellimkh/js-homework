63547ea95d8662f49b89aa2a542680732374950a
nellimkh <nellimkhoyan2005@gmail.com>
2025-10-19 21:21:37 +0400

    Homework

--- 16-10-2025/SingleNumber.js
+++ 16-10-2025/SingleNumber.js
@@ -11,28 +11,4 @@ var singleNumber = function(nums) {
 };
 
 
-console.log(singleNumber([2, 2, 1]));  /**
- * @param {number[]} height
- * @return {number}
- */
-var maxArea = function(height) {
-    let left = 0;
-    let right = height.length - 1;
-    let maxArea = 0;
-
-    while (left < right) {
-        const width = right - left;
-        const currentHeight = Math.min(height[left], height[right]);
-        const area = width * currentHeight;
-
-        maxArea = Math.max(maxArea, area);
-
-        if (height[left] < height[right]) {
-            left++;
-        } else {
-            right--;
-        }
-    }
-
-    return maxArea;
-};
+console.log(singleNumber([2, 2, 1])); 
